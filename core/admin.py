ce95172ec0b1d36c889bbb53e4564fa89d2976b4
Alexey <od-5@yandex.ru>
2015-08-06 09:00:50 +0300

    поиск по email в админке

--- core/admin.py
+++ core/admin.py
@@ -18,6 +18,7 @@ class TicketAdminForm(ModelForm):
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created', 'ticket_status', 'ticket_comment')
     list_filter = ['email', 'created', 'ticket_status']
+    search_fields = ['email', ]
     date_hierarchy = 'created'
     fields = ('name', 'email', 'comment', 'sale', 'ticket_status', 'ticket_comment')
     form = TicketAdminForm
@@ -85,6 +86,7 @@ class SaleAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'email', 'price', 'commission', 'total_price', 'sale_status', 'sale_comment')
     list_filter = ['travel_start', 'sale_status', ]
+    search_fields = ['email', ]
     date_hierarchy = 'travel_start'
     fields = ('name', 'email', 'comment', 'sale', 'sale_status', 'sale_comment', 'travel_start', 'travel_end', 'price', 'commission', 'total_price')
     form = SaleAdminForm

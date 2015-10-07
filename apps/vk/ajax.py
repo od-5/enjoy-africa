4cd22199eae1b8892a6634731a76967133691a74
Alexey <od-5@yandex.ru>
2015-10-07 23:15:19 +0300

    Merge branch 'master' of github.com:od-5/enjoy-africa
    Conflicts:
    	db.sqlite3

--- apps/vk/ajax.py
+++ apps/vk/ajax.py
@@ -10,18 +10,18 @@ def check(request):
         profile_id = request.POST.get('profile_id')
         name = u'%s %s' % (request.POST.get('last_name'), request.POST.get('first_name'))
         link = request.POST.get('link')
-        try:
-            exist_user = VkUser.objects.get(profile_id=int(profile_id))
-            exist_user.save()
-            return {
-                'success': u'Уже посещал'
-            }
-        except:
-            exist_user = VkUser(name=name, profile_id=int(profile_id), link=link)
-            exist_user.save()
-            return {
-                'success': u'Ещё не посещал!'
-            }
+        # try:
+        #     exist_user = VkUser.objects.get(profile_id=int(profile_id))
+        #     exist_user.save()
+        #     return {
+        #         'success': u'Уже посещал'
+        #     }
+        # except:
+        exist_user = VkUser(name=name, profile_id=int(profile_id), link=link)
+        exist_user.save()
+        return {
+            'success': u'Ещё не посещал!'
+        }
     else:
         return {
             'success': u'Ошибка!'
